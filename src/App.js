7599909a27cf8b1b1c6bc931ff3840d26a55511c
шамил амировkuhkjhkjhkjh <kulinec05@gmail.com>
2022-11-03 22:57:08 +0000

    Update 10 files

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-import {motion} from 'framer-motion'
+import React, { useState } from "react";
+import HorizontalLinearStepper from "./components/Stepper";
+
+// const imgData = ["child", "boy", "youngman", "grownman", "oldman"];
 
 function App() {
-  const [isVisible,setIsVisible] = useState(false)
+  const [activeStep, setActiveStep] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p  onClick={()=>setIsVisible(!isVisible)}>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <motion.div animate={{opacity:isVisible? 0.5 : 1}}>
-            dasdas
-            </motion.div>
-        </a>
-      </header>
-    </div>
+    <>
+      <HorizontalLinearStepper
+        activeStep={activeStep}
+        setActiveStep={setActiveStep}
+      />
+    </>
   );
 }
 
